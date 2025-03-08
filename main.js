3938527c4578985a0246a0a17051313b2625af52
kooldude62 <gramaa3805@g.comalisd.org>
2025-03-08 01:42:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,9 @@
   https://github.com/thatziv/webhook 
 */
 $(function(){
-    $('#btn').click(function(){
         var link = $('#link').val();
         var username = $('#username').val();
         var content = $('#content').val();
         var avatar = $('#avatar').val();
         $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
-
-    });
 });
